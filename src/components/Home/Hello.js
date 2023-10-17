97ec4e4ef4dc25ad0933c5a11262e05a991cacf6
renbrent <renbrent23@gmail.com>
2023-10-16 21:40:05 -0500

    added greeting

---
+++ src/components/Home/Hello.js
@@ -0,0 +1,14 @@
+import './Hello.css'
+
+export default function Hello() {
+    return (
+        <div className="grid">
+            <div className="greeting">
+                <h1>Hello World! <br/> I'm Brent Basiano!</h1>
+            </div>
+            <div className="pic">
+                <p>Show picture here.</p>
+            </div>
+        </div>
+    )
+}
